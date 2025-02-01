d5950593591eaebc855bd74e822dc044cceaffda
jackyk02 <jackykwok@berkeley.edu>
2025-02-01 06:11:50 +0000

    get cot & action seperately

--- cot_bench/speed.py
+++ cot_bench/speed.py
@@ -8,16 +8,11 @@ import os
 converter = TokenToAction()
 
 def batch(batch_size, temp):
-    SYSTEM_PROMPT = (
-    "A chat between a curious user and an artificial intelligence assistant. "
-    "The assistant gives helpful, detailed, and polite answers to the user's questions."
-    )
-
-    def get_openvla_prompt(instruction: str) -> str:
-        return f"{SYSTEM_PROMPT} USER: What action should the robot take to {instruction.lower()}? ASSISTANT: TASK:"
-
-    INSTRUCTION = "close the drawer"
-    prompt = get_openvla_prompt(INSTRUCTION)
+    instruction = "close the drawer"
+    prompt = "A chat between a curious user and an artificial intelligence assistant. " + \
+        "The assistant gives helpful, detailed, and polite answers to the user's questions. " + \
+        f"USER: What action should the robot take to {instruction.lower()}? ASSISTANT: TASK:"
+        
     arguments = [
         {
             "image_path": "images/robot.jpg",
@@ -29,13 +24,13 @@ def batch(batch_size, temp):
         max_new_tokens=1024,
         temperature=temp
     )
-    return [print(s) for s in states]
+    return [{'cot': s.text(), 'action': s.get_meta_info("action")["output_ids"][-8:-1]} for s in states]
 
 
 if __name__ == '__main__':
     #python -m sglang.launch_server --model-path openvla/openvla-7b --trust-remote-code --quantization fp8 --port 30000 --disable-radix-cach --dp 2 --tp 2
     sgl.set_default_backend(sgl.RuntimeEndpoint("http://localhost:30000"))
-    batch_size = 1
-    temperature = 0
+    batch_size = 2
+    temperature = 0.1
     actions = batch(batch_size=batch_size, temp=temperature)
     print(actions)
